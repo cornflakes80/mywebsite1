7a862bef4245a8dafea60c936993097171e890e0
cornflakes80 <henryboneswfh@gmail.com>
2025-09-27 05:39:51 -0400

    Delete My Website directory

--- My Website/myJava.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction1() { 
-  document.getElementById("demo1").innerHTML = "HTML stands for HyperText Markup Language";}
-
-function myFunction2() { 
-  document.getElementById("demo2").innerHTML = "CSS, which stands for Cascading Style Sheets, is a style sheet language used in web development to control the presentation and formatting of HTML documents. It allows developers to define the visual appearance of web pages, including aspects such as layout, colors, fonts, spacing, and responsive behavior for different devices.";}
-
-  function myFunction3() { 
-  document.getElementById("demo3").innerHTML = "JavaScript (JS) is a high-level, dynamic, and interpreted programming language primarily used to create interactive and dynamic content on web pages. While HTML provides the structure and CSS handles the styling, JavaScript adds behavior and functionality to websites.";}
